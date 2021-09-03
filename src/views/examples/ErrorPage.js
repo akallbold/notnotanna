b18f978b8c1db4aa3e709091281006b82a91cef8
AKB <a.kallenbornbolden@gmail.com>
2021-09-03 16:38:36 -0400

    small changes

--- src/views/examples/ErrorPage.js
+++ src/views/examples/ErrorPage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // reactstrap components
-import {  Container, Row } from "reactstrap";
-import {useLocation} from 'react-router-dom'
+import { Container, Row } from "reactstrap";
+import { useLocation } from "react-router-dom";
 // core components
 import SimpleNav from "components/Navbars/SimpleNav.js";
+import { isFirefox, isSafari } from "react-device-detect";
 
 function ErrorPage(props) {
   document.documentElement.classList.remove("nav-open");
@@ -17,11 +18,21 @@ function ErrorPage(props) {
     };
   });
 
-  const location = useLocation()  
-console.log("location.state", location.state)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const doesNavWork = () => {
+    if (isFirefox) return false;
+    if (isSafari) return false;
+    return true;
+  };
+
+  const location = useLocation();
+  console.log("location.state", location.state);
   return (
     <>
-      <SimpleNav/>
+      {doesNavWork ? <SimpleNav white /> : null}
       <div
         className="background-img"
         style={{
@@ -33,32 +44,40 @@ console.log("location.state", location.state)
         <Container>
           <Row>
             <h1 class="title">
-             {location.state && location.state.code ? location.state.code : "YIKES"}
-             </h1>
-              <br />
-              <p class="error-msg">{location.state && location.state.text ? location.state.text : "An error loading the error? Oh the irony!"}</p>
-              {location.state && location.state.link ? (
-             <div>
-             <br />
-              <a href={location.state.link} rel="noopener noreferrer">
-            <p>
-            Learn more here.
+              {location.state && location.state.code
+                ? location.state.code
+                : "YIKES"}
+            </h1>
+            <br />
+            <p
+              class="error-msg-akb"
+              style={{ color: "white", fontSize: "20px" }}
+            >
+              {location.state && location.state.text
+                ? location.state.text
+                : "An error loading the error? Oh the irony!"}
             </p>
-            </a>
-            </div>
-              )
-              : null}
-           
+            {location.state && location.state.link ? (
+              <div>
+                <br />
+                <a
+                  href={location.state.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <p>Learn more here.</p>
+                </a>
+              </div>
+            ) : null}
           </Row>
 
-          <Row >
+          {/* <Row >
             <a href="http://en.wikipedia.org/wiki/Special:Random" rel="noopener noreferrer">
             <h4>
             Sorry this didn't work out. Make the most of it and learn something new today.
             </h4>
             </a>
-          </Row>
-        
+          </Row> */}
         </Container>
       </div>
     </>
