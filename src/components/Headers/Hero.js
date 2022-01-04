65feead6ba9e012e0b86d29e650e21b3081afcb5
AKB <a.kallenbornbolden@gmail.com>
2022-01-03 20:04:38 -0500

    updated hero background

--- src/components/Headers/Hero.js
+++ src/components/Headers/Hero.js
@@ -1,23 +1,47 @@
 /*eslint-disable*/
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Container } from "reactstrap";
+import BIRDS from "vanta/dist/vanta.birds.min";
 
 function Hero() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          backgroundColor: 0xc3acac,
+          color1: 0x92931,
+          color2: 0xcacfd2,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
   return (
     <>
       <div className="wrapper">
         <div
-          className="page-header section-dark test"
-          style={{
-            backgroundImage: "url(" + require("assets/img/city.png") + ")",
-          }}
+          id="landing-background"
+          className="page-header section-dark"
+          ref={myRef}
         >
           <div className="content-center">
             <Container>
               <div className="title-brand">
                 <h1
-                  className="presentation-title test"
-                  style={{ fontSize: "5em", color: "white" }}
+                  style={{ fontSize: "5em", color: "grey", fontWeight: "bold" }}
                 >
                   Welcome to my site
                 </h1>
