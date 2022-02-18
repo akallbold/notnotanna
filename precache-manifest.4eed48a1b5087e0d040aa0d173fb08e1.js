c4ee96a42c0211c0484e92f8715e3b31708c46c0
AKB <a.kallenbornbolden@gmail.com>
2022-02-18 23:14:42 +0100

    Updates

---
+++ precache-manifest.4eed48a1b5087e0d040aa0d173fb08e1.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c80af75a6c8cee33ad0d8167837217",
+    "url": "/notnotanna/index.html"
+  },
+  {
+    "revision": "b14ed0a93554a3375372",
+    "url": "/notnotanna/static/css/main.6b015cf6.chunk.css"
+  },
+  {
+    "revision": "5eba17f1454edb69df19",
+    "url": "/notnotanna/static/js/2.70e68f34.chunk.js"
+  },
+  {
+    "revision": "f76b8096e654382ef2fdb9aa627b3b86",
+    "url": "/notnotanna/static/js/2.70e68f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14ed0a93554a3375372",
+    "url": "/notnotanna/static/js/main.8cc95ab2.chunk.js"
+  },
+  {
+    "revision": "b59e05489a70657c5f78340c9ce552f0",
+    "url": "/notnotanna/static/js/main.8cc95ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e341b9433bf0e4f45a",
+    "url": "/notnotanna/static/js/runtime-main.f811e074.js"
+  },
+  {
+    "revision": "8081725ce14560840507bbc2b610bb46",
+    "url": "/notnotanna/static/media/3b1b.8081725c.png"
+  },
+  {
+    "revision": "8b483dde49ac1bbd63435a52c7393e85",
+    "url": "/notnotanna/static/media/AWS-CloudPractitioner-2020.8b483dde.png"
+  },
+  {
+    "revision": "f5b1df9781088f65800f9524f00c2234",
+    "url": "/notnotanna/static/media/AWS-Developer-Associate-2020.f5b1df97.png"
+  },
+  {
+    "revision": "aa100f5af627232b89311fab7545ab26",
+    "url": "/notnotanna/static/media/Accenture-Logo.aa100f5a.png"
+  },
+  {
+    "revision": "e355bffebcf6baa8b1b0c7677f0e4b64",
+    "url": "/notnotanna/static/media/Pair_Programming.e355bffe.png"
+  },
+  {
+    "revision": "032eafc83a7096df27c33beada0e6607",
+    "url": "/notnotanna/static/media/agenda.032eafc8.png"
+  },
+  {
+    "revision": "4d4ad9159ec16626cf2cc48904608aed",
+    "url": "/notnotanna/static/media/angelo-pantazis.4d4ad915.jpg"
+  },
+  {
+    "revision": "dc3736ceced319d4db52770885933463",
+    "url": "/notnotanna/static/media/audi.dc3736ce.png"
+  },
+  {
+    "revision": "3023eb6c381ab629c29cb736e4e50d3e",
+    "url": "/notnotanna/static/media/ayo-ogunseinde-2.3023eb6c.jpg"
+  },
+  {
+    "revision": "56dbc1b5719a87dd40319a7107e2c10e",
+    "url": "/notnotanna/static/media/bottega.56dbc1b5.jpg"
+  },
+  {
+    "revision": "083424f9f775133afeef33f15901b1d9",
+    "url": "/notnotanna/static/media/bracelet.083424f9.jpg"
+  },
+  {
+    "revision": "b9d411f72adb0ba8407e332618b492a4",
+    "url": "/notnotanna/static/media/bulldog.b9d411f7.png"
+  },
+  {
+    "revision": "514a48f3059f062b30b64addc1595cda",
+    "url": "/notnotanna/static/media/ca.514a48f3.png"
+  },
+  {
+    "revision": "d6ec41143127e42e8bc1f1501dd64ef4",
+    "url": "/notnotanna/static/media/casio.d6ec4114.png"
+  },
+  {
+    "revision": "5a1ad76fd30b6c11f7d8d8050849ea44",
+    "url": "/notnotanna/static/media/cessna.5a1ad76f.png"
+  },
+  {
+    "revision": "b7b02093f4c7a3ddbdb33963c35621bd",
+    "url": "/notnotanna/static/media/clem-onojeghuo-2.b7b02093.jpg"
+  },
+  {
+    "revision": "1c3a9188a6ade0cca85f11a320482a59",
+    "url": "/notnotanna/static/media/coding-cat.1c3a9188.png"
+  },
+  {
+    "revision": "df1a1f56f39c890d143fc19645543ee0",
+    "url": "/notnotanna/static/media/coloured-card-with-btn.df1a1f56.png"
+  },
+  {
+    "revision": "616e469b919a32b9233b153e150acf4b",
+    "url": "/notnotanna/static/media/cover.616e469b.jpg"
+  },
+  {
+    "revision": "314c1b2585d16c588a6b6e4d4c155378",
+    "url": "/notnotanna/static/media/ct.314c1b25.png"
+  },
+  {
+    "revision": "5de7862ec951f2da937404c3ce77126f",
+    "url": "/notnotanna/static/media/daft-punk.5de7862e.png"
+  },
+  {
+    "revision": "13be385d4d1f97c81a66fa6a964398fd",
+    "url": "/notnotanna/static/media/default-skin.13be385d.svg"
+  },
+  {
+    "revision": "858fe65370dd1b1f35cdaaa3530b0c82",
+    "url": "/notnotanna/static/media/erik-lucatero-2.858fe653.jpg"
+  },
+  {
+    "revision": "58bf3276864338bd85cd2f71fa1450af",
+    "url": "/notnotanna/static/media/evernote.58bf3276.png"
+  },
+  {
+    "revision": "b8fba16048db7b7d0bcfef45a4f99057",
+    "url": "/notnotanna/static/media/evernote.b8fba160.png"
+  },
+  {
+    "revision": "0c111b046eff6393aabd3a89909c3b23",
+    "url": "/notnotanna/static/media/fabio-mangione.0c111b04.jpg"
+  },
+  {
+    "revision": "ba426eae7309c399ce0fcc36d2ab7348",
+    "url": "/notnotanna/static/media/fender.ba426eae.png"
+  },
+  {
+    "revision": "4995b463f039947a5c38d8849e95d7a0",
+    "url": "/notnotanna/static/media/flatiron.4995b463.png"
+  },
+  {
+    "revision": "c882587a648cdbd9ec4188ed4c035e81",
+    "url": "/notnotanna/static/media/gcp_cloud_arch.c882587a.png"
+  },
+  {
+    "revision": "40be6c400161fa9941a9d25c8d857ac8",
+    "url": "/notnotanna/static/media/gcp_cloud_eng.40be6c40.png"
+  },
+  {
+    "revision": "14509ac8202ab3e8745d72e006c02fdc",
+    "url": "/notnotanna/static/media/golf.14509ac8.png"
+  },
+  {
+    "revision": "980447c9595e2bd5f081e7dead420dfb",
+    "url": "/notnotanna/static/media/gucci-sun.980447c9.jpg"
+  },
+  {
+    "revision": "93f4cc86e29d283bda48a276d3b5cb95",
+    "url": "/notnotanna/static/media/hh.93f4cc86.png"
+  },
+  {
+    "revision": "43ebb8040aa4755063bf0a26bc0a2430",
+    "url": "/notnotanna/static/media/hydro-homie.43ebb804.png"
+  },
+  {
+    "revision": "17fd58c3331e6778b81c05238bed39fa",
+    "url": "/notnotanna/static/media/image_placeholder.17fd58c3.jpg"
+  },
+  {
+    "revision": "5800d0ee865351e2da31c8658e9a8541",
+    "url": "/notnotanna/static/media/jimi.5800d0ee.png"
+  },
+  {
+    "revision": "34203ce3d95f01c17fec5647e3188971",
+    "url": "/notnotanna/static/media/joe-gardner-2.34203ce3.jpg"
+  },
+  {
+    "revision": "d102021798d7640acdc4c8dc612ed786",
+    "url": "/notnotanna/static/media/kaci-baum-2.d1020217.jpg"
+  },
+  {
+    "revision": "59972b4c4b3b7fa918a7a6aef2950aa7",
+    "url": "/notnotanna/static/media/laptop-basic.59972b4c.png"
+  },
+  {
+    "revision": "90cb0947a30fba4705866c8524143458",
+    "url": "/notnotanna/static/media/martin-knize.90cb0947.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/notnotanna/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/notnotanna/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/notnotanna/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/notnotanna/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "5fd69a91442ac6c5cf51b2a734f01b35",
+    "url": "/notnotanna/static/media/nyc.5fd69a91.png"
+  },
+  {
+    "revision": "9854ad9ddce30f93007919808a3f9124",
+    "url": "/notnotanna/static/media/outfit-burberry.9854ad9d.jpg"
+  },
+  {
+    "revision": "c466038b3fecf2c57f2f2b0af7eb010e",
+    "url": "/notnotanna/static/media/outfit-gucci.c466038b.jpg"
+  },
+  {
+    "revision": "9e48fe54e2d9a747824d372a879993e1",
+    "url": "/notnotanna/static/media/outfit-maison-margiela.9e48fe54.jpg"
+  },
+  {
+    "revision": "7c9e3d0519bc0a9423d084f49856e70a",
+    "url": "/notnotanna/static/media/outfit-paul-smith.7c9e3d05.jpg"
+  },
+  {
+    "revision": "496be5c6d0f09e1dcf2718661863e72b",
+    "url": "/notnotanna/static/media/pavel-kosov.496be5c6.jpg"
+  },
+  {
+    "revision": "2df395cb209a51aa097d1ab9bd89e9f8",
+    "url": "/notnotanna/static/media/pedro-lastra.2df395cb.jpg"
+  },
+  {
+    "revision": "f20b3410b5c060d70fadbfe6306eac78",
+    "url": "/notnotanna/static/media/plant.f20b3410.png"
+  },
+  {
+    "revision": "3862ba85077655be362a17707b1f5854",
+    "url": "/notnotanna/static/media/por7o.3862ba85.jpg"
+  },
+  {
+    "revision": "539d396228fa3627a29f2683d06a5a98",
+    "url": "/notnotanna/static/media/rawpixel-coms.539d3962.jpg"
+  },
+  {
+    "revision": "4c007de63c109600f714d48c6e30140b",
+    "url": "/notnotanna/static/media/ressence.4c007de6.jpg"
+  },
+  {
+    "revision": "9bc5d441e8cf9f10dab8887909277dff",
+    "url": "/notnotanna/static/media/sfdc-admin-badge.9bc5d441.png"
+  },
+  {
+    "revision": "6570b532519c03b64759ea2daf6eccc9",
+    "url": "/notnotanna/static/media/sfdc-srvc-badge.6570b532.png"
+  },
+  {
+    "revision": "74f5520f011c2653f98b166337c2796e",
+    "url": "/notnotanna/static/media/soroush-karimi.74f5520f.jpg"
+  },
+  {
+    "revision": "3ae203b14343258796f3425ff40615f4",
+    "url": "/notnotanna/static/media/ss-badge.3ae203b1.png"
+  },
+  {
+    "revision": "044e5eff90db6b29fcbd5773488dc740",
+    "url": "/notnotanna/static/media/table.044e5eff.jpg"
+  },
+  {
+    "revision": "15ea02c8d3fb5f0987be3c4d6db5cad0",
+    "url": "/notnotanna/static/media/tennis.15ea02c8.png"
+  },
+  {
+    "revision": "2905daff3dea4147397381b462c54655",
+    "url": "/notnotanna/static/media/uconn.2905daff.png"
+  },
+  {
+    "revision": "980e833394c58e4cc69436228c334328",
+    "url": "/notnotanna/static/media/usa.980e8333.png"
+  },
+  {
+    "revision": "ffe946fe40b7294078524df347cba067",
+    "url": "/notnotanna/static/media/yale.ffe946fe.png"
+  }
+]);
--- src/data/stylingConstants.js
+++
@@ -1,5 +0,0 @@
-export const containerHeights = {
-  small: "10px",
-  medium: "20px",
-  large: "50px",
-};
